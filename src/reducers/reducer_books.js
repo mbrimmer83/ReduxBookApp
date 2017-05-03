b426ca0edbf45ffa61bf5a522f15311ee0f2fa27
Matt Brimmer <mbrimmer@ATL-MBRIMMER.local>
2017-05-03 15:34:51 -0400

    Initial Redux Book Application commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+  return [
+    {title: 'Javascipt: The Good Parts', pages: 101},
+    {title: 'Harry Potter', pages: 566},
+    {title: 'The Dark Tower', pages: 320},
+    {title: 'Node JS in Action', pages: 400},
+    {title: 'Machine Learning in Python', pages: 123}
+  ]
+}
